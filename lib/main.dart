c40a02348fe093c78f8b2067757e62ff92a4e6ac
arya-rgb <dickyarya.personal@gmail.com>
2021-06-30 17:17:21 +0700

    add detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,13 +11,68 @@ class MyApp extends StatelessWidget {
       home: DetailScren(),
     );
   }
-  // This widget is the root of your application.
+// This widget is the root of your application.
 }
+
 class DetailScren extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: Text(
+                  'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text("Open Everyday"),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.alarm),
+                      SizedBox(height: 8.0),
+                      Text("09-00 - 20-00"),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.money),
+                      SizedBox(height: 8.0),
+                      Text("Rp 25.000"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
